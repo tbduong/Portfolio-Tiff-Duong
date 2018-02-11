7ec8b4902c427bb1cff48985571944b238477b83
tbduong <tiffaniduong@gmail.com>
2018-02-11 15:13:18 -0800

    npm implement

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
+
 $( document ).ready(function() {
 
       $(window).scroll(function(){
