0a884fff7f1971658ebf9020f9d9edb5ceafe89a
tbduong <tiffaniduong@gmail.com>
2018-02-11 16:23:19 -0800

    change listening

--- js/main.js
+++ js/main.js
@@ -1,6 +1,51 @@
-server.listen(config.port, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-});
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (request, response) {
+
+   console.log('request starting for ');
+   console.log(request);
+
+   var filePath = '.' + request.url;
+   if (filePath == './')
+       filePath = './index.html';
+
+   console.log(filePath);
+   var extname = path.extname(filePath);
+   var contentType = 'text/html';
+   switch (extname) {
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.css':
+           contentType = 'text/css';
+           break;
+   }
+
+   path.exists(filePath, function(exists) {
+
+       if (exists) {
+           fs.readFile(filePath, function(error, content) {
+               if (error) {
+                   response.writeHead(500);
+                   response.end();
+               }
+               else {
+                   response.writeHead(200, { 'Content-Type': contentType });
+                   response.end(content, 'utf-8');
+               }
+           });
+       }
+       else {
+           response.writeHead(404);
+           response.end();
+       }
+   });
+
+}).listen(process.env.PORT || 5000);
+
+console.log('Server running at http://127.0.0.1:5000/');
 
 $( document ).ready(function() {
 
